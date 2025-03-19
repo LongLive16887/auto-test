e88bb24517b862d1ab9efa89331078a1f38dfeed
zayn <zayn@ru>
2025-03-20 01:06:05 +0500

    add news

--- src/store/cards.tsx
+++ src/store/cards.tsx
@@ -51,8 +51,8 @@ export const useCardStore = create<EditStore>()(
           const { page, data } = get()
           const url = id
             ? type === 'group'
-              ? `/api/v1/question?groupId=${id}`
-              : `/api/v1/question?lessonId=${id}`
+              ? `/api/v1/question?lessonId=${id}`
+              : `/api/v1/question?groupId=${id}`
             : `/api/v1/question?page=${page}&size=10`
           const res = await api.get(url)
           const newData = res.data.data.results
